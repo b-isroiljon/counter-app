6cac157abc3f18d9b8e2a71e5853ba43d380c6cb
Boruev Isroiljon <140053898+b-isroiljon@users.noreply.github.com>
2024-02-14 23:59:19 +0600

    decrement buttons

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,6 +14,7 @@ class Counters extends Component {
             key={counter.id}
             onDelete={onDelete}
             onIncrement={onIncrement}
+            onDecrement={this.props.onDecrement}
             counter={counter}
           />
         ))}
